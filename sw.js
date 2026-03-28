b2bedd35fcfe41f8d23d86bb6a0d8dbb760785f7
Javierg722 <89281954+Javierg722@users.noreply.github.com>
2026-03-28 15:50:10 -0500

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE_NAME = "sale-tracker-pwa-v3";
-const ASSETS = ["./","./index.html","./css/styles.css","./js/app.js","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png"];
-self.addEventListener("install", e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))));
-self.addEventListener("fetch", e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
